35f8664c526bd5a2c9f3c8f58c40ac3d81f4d907
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-16 11:55:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "21803165"))
 API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
+OWNER_ID = int(getenv("OWNER_ID", "6625936112"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "WORLD_ALPHA")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "ll_Bot_Support_ll")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "ll_Bot_Promotion_ll")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "lll_Oye_Zayn_lll")
 
 # Random Start Images
 IMG = [
